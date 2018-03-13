8d9ded6894697e9cc03f5a16c16d39efee83c35c
Nikita Kirsanov <nikita_kirsanov@epam.com>
2018-03-13 23:34:50 +0300

    :bug: do not inline images

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -38,7 +38,7 @@ module.exports = {
       loader: "style-loader!css-loader"
     }, {
       test: /\.(png|jpg|gif)$/,
-      loader: "url-loader?limit=8192"
+      loader: "url-loader?limit=1"
     }, {
       test: /\.svg$/,
       loader: "url-loader?limit=10000&mimetype=image/svg+xml"
